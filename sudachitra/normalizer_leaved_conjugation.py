44556ecef9b63d49f20172f577cd85b7123fa2b8
katsuta_a <katsuta_a@worksap.co.jp>
2021-12-10 20:01:16 +0900

    change list to tuple and change key from str to tuple

--- sudachitra/normalizer_leaved_conjugation.py
+++ sudachitra/normalizer_leaved_conjugation.py
@@ -1,43 +1,56 @@
 
 import json
 from sudachipy.morpheme import Morpheme
+from sudachipy.sudachipy import Dictionary
 
 from sudachitra.tokenization_bert_sudachipy import CONJUGATIVE_POS
 
 
 class NormalizerLeavedConjugation:
-    def __init__(self, inflectoin_table_path, conjugation_type_table_path) -> None:
-        with open(inflectoin_table_path) as jf:
-            self.infl_table = json.load(jf)
-        with open(conjugation_type_table_path) as jf:
-            self.conj_type_table = json.load(jf)
-        self.errlog = open("err.log", "w")
+    def __init__(self, inflection_table_path, conjugation_type_table_path, sudachi_dict: Dictionary) -> None:
+        self.sudachi_dict = sudachi_dict
+        self.is_conjugative_pos = sudachi_dict.pos_matcher(lambda p: p[0] in CONJUGATIVE_POS)
+        self.is_needs_inflection = sudachi_dict.pos_matcher(lambda p: p[4] == "サ行変格" or p[5] != "終止形-一般") # 「為る->する」のため、サ行変格のみ 終止形-一般も変化
 
-    def _read_pos(self, morpheme: Morpheme) -> list:
-        pos = list(morpheme.part_of_speech())
-        key = "|".join((morpheme.surface(), morpheme.reading_form(),
-                       morpheme.normalized_form(), pos[4]))
-        if key in self.conj_type_table[pos[0]]:
-            pos[4] = self.conj_type_table[pos[0]][key]
-            if pos[4]+"|"+pos[5] not in self.infl_table[pos[0]]:
-                pos[5] = pos[5].split("-")[0] + "-一般"
-        return pos
+        with open(inflection_table_path) as jf:
+            self.infl_table = self._load_json(jf)
+        with open(conjugation_type_table_path) as jf:
+            self.conj_type_table = self._load_json(jf)
+    
+    def _load_json(self, json_file) -> dict:
+        data = json.load(json_file)
+        table = {(pos_0, *key.split("|")): convert_table for pos_0, convert_tables in data.items() for key, convert_table in convert_tables.items()}
+        return table
+
+    def _change_pos(self, key, pos) -> tuple:
+        conj_type = self.conj_type_table[key]
+        conj_form = pos[5]
+        if (pos[0], conj_type, conj_form) not in self.infl_table:
+            conj_form = pos[5].split("-")[0] + "-一般"
+        return (*pos[:4], conj_type, conj_form)
+    
+    def _is_changed_conjugation_type(self, key) -> bool:
+        return key in self.conj_type_table
 
     def normalized(self, morpheme: Morpheme) -> str:
         normalized_token = morpheme.normalized_form()
-        if morpheme.part_of_speech()[0] not in CONJUGATIVE_POS:
+        if not self.is_conjugative_pos(morpheme) or not self.is_needs_inflection(morpheme):
             return normalized_token
 
-        pos = self._read_pos(morpheme)
-        if pos[5] != '終止形-一般' or pos[4] == "サ行変格":  # 「為る->する」のため、サ行変格のみ 終止形-一般も変化
-            convert_tables = self.infl_table[pos[0]][pos[4]+"|"+pos[5]]
-            for convert_table in convert_tables:
-                if convert_table == ['', '']:
-                    return normalized_token
-                if normalized_token.endswith(convert_table[0]):
-                    src = convert_table[0][::-1]
-                    tgt = convert_table[1][::-1]
-                    return normalized_token[::-1].replace(src, tgt, 1)[::-1]
+        pos = morpheme.part_of_speech()
+        conj_type_table_key = (pos[0], morpheme.surface(), morpheme.reading_form(), normalized_token, pos[4])
+        if self._is_changed_conjugation_type(conj_type_table_key):
+            pos = self._change_pos(conj_type_table_key, pos)
+            if pos[5] != "終止形-一般":
+                return normalized_token
+
+        for convert_table in self.infl_table[(pos[0], pos[4], pos[5])]:
+            if convert_table == ['', '']:
+                return normalized_token
+            if normalized_token.endswith(convert_table[0]):
+                src = convert_table[0][::-1]
+                tgt = convert_table[1][::-1]
+                return normalized_token[::-1].replace(src, tgt, 1)[::-1]
 
         return normalized_token
 
