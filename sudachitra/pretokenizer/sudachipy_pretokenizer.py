1b818ea4308b224ebc4c25b194b9d9b4906073c3
t-yamamura <takash1_yamamura@yahoo.co.jp>
2022-01-21 19:15:41 +0900

    use morpheme indexes instead of pytextspan

--- sudachitra/pretokenizer/sudachipy_pretokenizer.py
+++ sudachitra/pretokenizer/sudachipy_pretokenizer.py
@@ -20,21 +20,6 @@ from typing import Callable, List, Optional
 from ..word_formatter import word_formatter, WordFormTypes
 
 
-def split_normalized_string(normalized_string: NormalizedString, tokens: List[str]) -> List[NormalizedString]:
-    """
-    Splits normalized_string by tokens.
-
-    Args:
-        normalized_string (NormalizedString): Input string.
-        tokens (List[str]): List of surface words in a sentence.
-
-    Returns:
-        List[NormalizedString]: List of normalized_strings.
-    """
-    token_spans = textspan.get_original_spans(tokens, str(normalized_string))
-    return [normalized_string[start:end] for token_span in token_spans for start, end in token_span]
-
-
 def pretokenizer_handler(sudachi_dict: Dictionary, word_form_type: Optional[str] = 'surface')\
         -> Callable[[int, NormalizedString, MorphemeList], List[NormalizedString]]:
     """
@@ -58,7 +43,8 @@ def pretokenizer_handler(sudachi_dict: Dictionary, word_form_type: Optional[str]
 
     def _handler(index: int, original: NormalizedString, morphemes: MorphemeList) -> List[NormalizedString]:
         tokens = [m.surface() for m in morphemes if m.surface() != '']
-        normalized_strings = split_normalized_string(original, tokens)
+        normalized_strings = [original[m.begin():m.end()] for m in morphemes]
+
         if len(tokens) != len(normalized_strings):
             raise ValueError(len(morphemes), len(tokens), len(normalized_strings), tokens, morphemes, normalized_strings)
         if word_form_type != WordFormTypes.SURFACE:
