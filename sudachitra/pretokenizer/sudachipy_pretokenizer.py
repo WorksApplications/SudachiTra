d43d1adffcc24431209dba5f79f81894cb09c1a7
t-yamamura <takash1_yamamura@yahoo.co.jp>
2022-01-21 20:38:07 +0900

    remove pytextspan import

--- sudachitra/pretokenizer/sudachipy_pretokenizer.py
+++ sudachitra/pretokenizer/sudachipy_pretokenizer.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import textspan
 from sudachipy import Dictionary, MorphemeList
 from tokenizers import NormalizedString
 from typing import Callable, List, Optional
