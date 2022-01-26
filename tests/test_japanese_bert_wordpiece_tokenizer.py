66afb37e3d4dadd4172dc44c24690008b54f5511
t-yamamura <takash1_yamamura@yahoo.co.jp>
2022-01-26 17:24:13 +0900

    add tests for encoding properties

--- tests/test_japanese_bert_wordpiece_tokenizer.py
+++ tests/test_japanese_bert_wordpiece_tokenizer.py
@@ -12,12 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import re
 import unittest
+from typing import List
 
 from sudachipy import Dictionary, SplitMode
 from sudachitra.pretokenizer import JapaneseBertWordPieceTokenizer
 from sudachitra.pretokenizer import pretokenizer_handler
 from sudachitra.word_formatter import WordFormTypes
+from tokenizers import Encoding
 
 
 class JapaneseBertWordPieceTokenizerTest(unittest.TestCase):
@@ -44,10 +47,12 @@ class JapaneseBertWordPieceTokenizerTest(unittest.TestCase):
             "Sudachi": 17,
             "sudachi": 18,
             "を": 19,
-            "とどけ": 20,
-            "とどける": 21,
-            "届け": 22,
-            "届ける": 23,
+            "とど": 20,
+            "届": 21,
+            "##け": 22,
+            "##る": 23,
+            # "届け": 22,
+            # "届ける": 23,
             "ます": 24,
             "…": 25,
             ".": 26,
@@ -58,9 +63,14 @@ class JapaneseBertWordPieceTokenizerTest(unittest.TestCase):
                                                            do_lower_case=False,
                                                            do_nfkc=False,
                                                            do_strip=False)
+        self.wordpieces_prefix = '##'
+        self.unk_token = '[UNK]'
+        self.prefix_pattern = re.compile(f'^{self.wordpieces_prefix}')
+        self.delete_prefix = lambda x: self.prefix_pattern.sub('', x)
 
         self.sudachi_dict = Dictionary(dict='core')
         self.test_sentence = '引越してからすだち－Ｓｕｄａｃｈｉをとどけます。'
+        self.sudachi = self.sudachi_dict.create(mode=SplitMode.C)
 
     def set_pretokenizer(self, word_form_type: WordFormTypes):
         pretok = self.sudachi_dict.pre_tokenizer(mode=SplitMode.C,
@@ -68,89 +78,104 @@ class JapaneseBertWordPieceTokenizerTest(unittest.TestCase):
                                                                               word_form_type=word_form_type))
         self.wp_tokenizer.pre_tokenizer = pretok
 
+    def validate_encoding(self, tokens: List[str], encoding: Encoding):
+        """
+        Validates properties of `Encoding`.
+        (https://github.com/huggingface/tokenizers/blob/master/bindings/python/py_src/tokenizers/__init__.pyi#L69)
+
+        Args:
+            tokens (List[str]): Expected tokens.
+            encoding (Encoding): Encoded tokens.
+        """
+        self.assertListEqual(tokens, encoding.tokens)
+        self.assertListEqual(list(map(self.wp_tokenizer.token_to_id, tokens)), encoding.ids)
+        self.assertListEqual([None, *[0 for _ in range(len(tokens) - 2)], None], encoding.sequence_ids)
+        self.assertListEqual([1, *[0 for _ in range(len(tokens) - 2)], 1], encoding.special_tokens_mask)
+        self.assertListEqual([0 for _ in range(len(tokens))], encoding.type_ids)
+        self.assertListEqual([1 for _ in range(len(tokens))], encoding.attention_mask)
+        # ToDo: add test for encoding.offsets and encoding.word_ids (https://github.com/WorksApplications/SudachiTra/issues/42)
+
     def test_surface(self):
-        self.set_pretokenizer(WordFormTypes.SURFACE)
+        word_form_type = WordFormTypes.SURFACE
+        tokens = ['[CLS]', '引越', 'し', 'て', 'から', 'すだち', '－', 'Ｓｕｄａｃｈｉ', 'を', 'とど', '##け', 'ます', '。', '[SEP]']
 
-        res = self.wp_tokenizer.encode(self.test_sentence)
-
-        tokens = ['[CLS]', '引越', 'し', 'て', 'から', 'すだち', '－', 'Ｓｕｄａｃｈｉ', 'を', 'とどけ', 'ます', '。', '[SEP]']
-        self.assertListEqual(tokens, res.tokens)
-        self.assertListEqual(list(map(self.wp_tokenizer.token_to_id, tokens)), res.ids)
+        self.set_pretokenizer(word_form_type)
+        encoding = self.wp_tokenizer.encode(self.test_sentence)
+        self.validate_encoding(tokens, encoding)
 
     def test_normalized_and_surface(self):
-        self.set_pretokenizer(WordFormTypes.NORMALIZED_AND_SURFACE)
+        word_form_type = WordFormTypes.NORMALIZED_AND_SURFACE
+        tokens = ['[CLS]', '引っ越し', 'し', 'て', 'から', '酢橘', '－', 'Sudachi', 'を', 'とど', '##け', 'ます', '。', '[SEP]']
 
-        res = self.wp_tokenizer.encode(self.test_sentence)
-
-        tokens = ['[CLS]', '引っ越し', 'し', 'て', 'から', '酢橘', '－', 'Sudachi', 'を', 'とどけ', 'ます', '。', '[SEP]']
-        self.assertListEqual(tokens, res.tokens)
-        self.assertListEqual(list(map(self.wp_tokenizer.token_to_id, tokens)), res.ids)
+        self.set_pretokenizer(word_form_type)
+        encoding = self.wp_tokenizer.encode(self.test_sentence)
+        self.validate_encoding(tokens, encoding)
 
     def test_normalized_conjugation(self):
-        self.set_pretokenizer(WordFormTypes.NORMALIZED_CONJUGATION)
+        word_form_type = WordFormTypes.NORMALIZED_CONJUGATION
+        tokens = ['[CLS]', '引っ越し', 'し', 'て', 'から', '酢橘', '－', 'Sudachi', 'を', '届', '##け', 'ます', '。', '[SEP]']
 
-        res = self.wp_tokenizer.encode(self.test_sentence)
-        tokens = ['[CLS]', '引っ越し', 'し', 'て', 'から', '酢橘', '－', 'Sudachi', 'を', '届け', 'ます', '。', '[SEP]']
-
-        self.assertListEqual(tokens, res.tokens)
-        self.assertListEqual(list(map(self.wp_tokenizer.token_to_id, tokens)), res.ids)
+        self.set_pretokenizer(word_form_type)
+        encoding = self.wp_tokenizer.encode(self.test_sentence)
+        self.validate_encoding(tokens, encoding)
 
     def test_normalized_form(self):
-        self.set_pretokenizer(WordFormTypes.NORMALIZED)
+        word_form_type = WordFormTypes.NORMALIZED
+        tokens = ['[CLS]', '引っ越し', '為る', 'て', 'から', '酢橘', '－', 'Sudachi', 'を', '届', '##け', '##る', 'ます', '。', '[SEP]']
 
-        res = self.wp_tokenizer.encode(self.test_sentence)
-
-        tokens = ['[CLS]', '引っ越し', '為る', 'て', 'から', '酢橘', '－', 'Sudachi', 'を', '届ける', 'ます', '。', '[SEP]']
-        self.assertListEqual(tokens, res.tokens)
-        self.assertListEqual(list(map(self.wp_tokenizer.token_to_id, tokens)), res.ids)
+        self.set_pretokenizer(word_form_type)
+        encoding = self.wp_tokenizer.encode(self.test_sentence)
+        self.validate_encoding(tokens, encoding)
 
     def test_dictionary_form(self):
-        self.set_pretokenizer(WordFormTypes.DICTIONARY)
+        word_form_type = WordFormTypes.DICTIONARY
+        tokens = ['[CLS]', '引越', 'する', 'て', 'から', 'すだち', '－', 'Sudachi', 'を', 'とど', '##け', '##る', 'ます', '。', '[SEP]']
 
-        res = self.wp_tokenizer.encode(self.test_sentence)
-
-        tokens = ['[CLS]', '引越', 'する', 'て', 'から', 'すだち', '－', 'Sudachi', 'を', 'とどける', 'ます', '。', '[SEP]']
-        self.assertListEqual(tokens, res.tokens)
-        self.assertListEqual(list(map(self.wp_tokenizer.token_to_id, tokens)), res.ids)
+        self.set_pretokenizer(word_form_type)
+        encoding = self.wp_tokenizer.encode(self.test_sentence)
+        self.validate_encoding(tokens, encoding)
 
     def test_dictionary_and_surface(self):
-        self.set_pretokenizer(WordFormTypes.DICTIONARY_AND_SURFACE)
+        word_form_type = WordFormTypes.DICTIONARY_AND_SURFACE
+        tokens = ['[CLS]', '引越', 'し', 'て', 'から', 'すだち', '－', 'Sudachi', 'を', 'とど', '##け', 'ます', '。', '[SEP]']
 
-        res = self.wp_tokenizer.encode(self.test_sentence)
-        tokens = ['[CLS]', '引越', 'し', 'て', 'から', 'すだち', '－', 'Sudachi', 'を', 'とどけ', 'ます', '。', '[SEP]']
-        self.assertListEqual(tokens, res.tokens)
-        self.assertListEqual(list(map(self.wp_tokenizer.token_to_id, tokens)), res.ids)
+        self.set_pretokenizer(word_form_type)
+        encoding = self.wp_tokenizer.encode(self.test_sentence)
+        self.validate_encoding(tokens, encoding)
 
     def test_normalizers(self):
         self.wp_tokenizer = JapaneseBertWordPieceTokenizer(vocab=self.vocab,
                                                            do_lower_case=True,
                                                            do_nfkc=True,
                                                            do_strip=True)
-        self.set_pretokenizer(WordFormTypes.SURFACE)
+        word_form_type = WordFormTypes.SURFACE
+        self.set_pretokenizer(word_form_type)
 
         # lowercase
-        res = self.wp_tokenizer.encode('SUDACHI')
+        sentence = 'SUDACHI'
+        encoding = self.wp_tokenizer.encode(sentence)
         tokens = ['[CLS]', 'sudachi', '[SEP]']
-        self.assertListEqual(tokens, res.tokens)
-        self.assertListEqual(list(map(self.wp_tokenizer.token_to_id, tokens)), res.ids)
+        self.validate_encoding(tokens, encoding)
 
-        # strip
-        res = self.wp_tokenizer.encode(' sudachi\n')
+        # # strip
+        sentence = ' sudachi\n'
+        encoding = self.wp_tokenizer.encode(sentence)
         tokens = ['[CLS]', 'sudachi', '[SEP]']
-        self.assertListEqual(tokens, res.tokens)
-        self.assertListEqual(list(map(self.wp_tokenizer.token_to_id, tokens)), res.ids)
+        self.validate_encoding(tokens, encoding)
+        self.validate_encoding(tokens, encoding)
 
         # nfkc
-        res = self.wp_tokenizer.encode('…')
+        sentence = '…'
+        encoding = self.wp_tokenizer.encode(sentence)
         tokens = ['[CLS]', '.', '.', '.', '[SEP]']
-        self.assertListEqual(tokens, res.tokens)
-        self.assertListEqual(list(map(self.wp_tokenizer.token_to_id, tokens)), res.ids)
+        self.validate_encoding(tokens, encoding)
+        self.validate_encoding(tokens, encoding)
 
     def test_oov(self):
-        self.set_pretokenizer(WordFormTypes.SURFACE)
+        word_form_type = WordFormTypes.SURFACE
+        self.set_pretokenizer(word_form_type)
 
-        res = self.wp_tokenizer.encode('OOV')
-        tokens = ['[CLS]', '[UNK]', '[SEP]']
-        self.assertListEqual(tokens, res.tokens)
-        self.assertListEqual(list(map(self.wp_tokenizer.token_to_id, tokens)), res.ids)
-
+        sentence = 'OOV酢橘OOV酢橘OOV'
+        encoding = self.wp_tokenizer.encode(sentence)
+        tokens = ['[CLS]', '[UNK]', '酢橘', '[UNK]', '酢橘', '[UNK]', '[SEP]']
+        self.validate_encoding(tokens, encoding)
